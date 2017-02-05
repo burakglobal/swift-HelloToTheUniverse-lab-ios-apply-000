f0cc5168b291b599139cca2eda61ed33183c8d8c
burakflatiron <burakflatiron@Tech-Book.home>
2017-02-05 10:15:20 -0500

    Finished this lovely lab

--- HelloToTheUniverse/Greeting.swift
+++ HelloToTheUniverse/Greeting.swift
@@ -15,7 +15,7 @@ class Greeting {
     // We created the function for you! Press command + u to run the tests.
     
     func helloUniverse() -> String {
-        Print("Burak Kebapci")
+        print("Burak Kebapci")
         return "Hello Universe!"
     }
     
